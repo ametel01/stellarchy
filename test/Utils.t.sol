d14e84db2b054f20b05cdebf28ce353ef54f5b0d
ametel01 <alex-metelli@gmx.com>
2023-07-14 13:35:40 +0800

    test: added setUp()

---
+++ test/Utils.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+
+import "../src/tokens/STERC721.sol";
+import "../src/tokens/STERC20.sol";
+import "../src/Stellarchy.sol";
+
+contract TestSetup is Test {
+    STERC721 internal erc721;
+    STERC20 internal steel;
+    STERC20 internal quartz;
+    STERC20 internal tritium;
+    Stellarchy internal game;
+
+    function setUp() public {
+        erc721 = new STERC721();
+        steel = new STERC20("Stellarchy Steel", "SST");
+        quartz = new STERC20("Stellarchy Quartz", "SQZ");
+        tritium = new STERC20("Stellarchy Tritium", "STT");
+        game = new Stellarchy(address(erc721), address(steel), address(quartz), address(tritium));
+
+        erc721.setMinter(address(game));
+        steel.setMinter(address(game));
+        quartz.setMinter(address(game));
+        tritium.setMinter(address(game));
+    }
+}
