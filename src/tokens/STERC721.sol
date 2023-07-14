fce351dd023ea05ad8a0750f851266b332cb2cec
ametel01 <alex-metelli@gmx.com>
2023-07-14 13:35:22 +0800

    fix wrong function arguments

--- src/tokens/STERC721.sol
+++ src/tokens/STERC721.sol
@@ -5,21 +5,17 @@ import "openzeppelin/token/ERC721/ERC721.sol";
 import "openzeppelin/access/Ownable.sol";
 
 interface ISTERC721 is IERC721 {
-    function tokenOf(uint256 tokenId) external view returns (address);
+    function tokenOf(address account) external view returns (uint256 tokeId);
     function mint(address to, uint256 tokenId) external;
 }
 
 contract STERC721 is ERC721("Stellarchy Planet", "STPL"), Ownable {
     address minter;
 
-    mapping(uint256 => address) tokens;
+    mapping(address => uint256) tokens;
 
-    constructor(address _minter) {
-        minter = _minter;
-    }
-
-    function tokenOf(uint256 tokenId) external view returns (address) {
-        return tokens[tokenId];
+    function tokenOf(address account) external view returns (uint256 tokenId) {
+        return tokens[account];
     }
 
     function setMinter(address _minter) external virtual onlyOwner {
@@ -29,7 +25,7 @@ contract STERC721 is ERC721("Stellarchy Planet", "STPL"), Ownable {
     function mint(address to, uint256 tokenId) external virtual {
         require(msg.sender == minter, "caller is not minter");
         require(balanceOf(to) == 0, "max planets per address is 1");
-        tokens[tokenId] = to;
+        tokens[to] = tokenId;
         _safeMint(to, tokenId);
     }
 }
