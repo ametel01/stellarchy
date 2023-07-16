932a8edf00b22a83f3e8d4f0102d408d3a5c8ad2
ametel01 <alex-metelli@gmx.com>
2023-07-16 10:40:40 +0800

    added helper for test

--- test/Setup.t.sol
+++ test/Setup.t.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19 .0;
 
+import "forge-std/console.sol";
 import "forge-std/Test.sol";
 
 import {STERC721} from "../src/tokens/STERC721.sol";
 import {STERC20} from "../src/tokens/STERC20.sol";
 import {Stellarchy} from "../src/Stellarchy.sol";
+import {Structs} from "../src/libraries/Structs.sol";
 
-contract TestSetup is Test {
+contract TestSetup is Test, Structs {
     uint256 public constant ONE_DAY = 86400;
     STERC721 internal erc721;
     STERC20 internal steel;
@@ -31,5 +33,32 @@ contract TestSetup is Test {
         steel.setMinter(address(game));
         quartz.setMinter(address(game));
         tritium.setMinter(address(game));
+    }
+
+    function testSetUp() public returns (address) {
+        address p1 = vm.addr(0x1);
+        deal(p1, 1 ether);
+        address p2 = vm.addr(0x2);
+        deal(p2, 1 ether);
+        vm.startPrank(p1);
+        game.generatePlanet{value: 0.01 ether}();
+        game.energyPlantUpgrade();
+        game.steelMineUpgrade();
+        game.quartzMineUpgrade();
+        game.energyPlantUpgrade();
+        game.steelMineUpgrade();
+        vm.warp(ONE_DAY);
+        game.steelMineUpgrade();
+        game.quartzMineUpgrade();
+        game.energyPlantUpgrade();
+        game.tritiumMineUpgrade();
+        game.tritiumMineUpgrade();
+        game.energyPlantUpgrade();
+        vm.warp(ONE_DAY*100000000);
+        game.collectResources();
+        // ERC20s memory resources = game.getSpendableResources(1); 
+        // console.log(resources.steel, resources.quartz, resources.tritium);
+        return p1;
+
     }
 }
