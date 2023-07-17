618d2ffe6df543d5b2a5408eb39c010aae74c229
ametel01 <alex-metelli@gmx.com>
2023-07-17 08:57:22 +0800

    changed functions visibility

--- src/Defences.sol
+++ src/Defences.sol
@@ -12,12 +12,12 @@ contract Defences {
 
     mapping(uint256 => uint256) public plasmaAvailable;
 
-    function _getDefencesCost(
+    function getDefencesCost(
         uint256 quantity,
         uint256 _steel,
         uint256 _quartz,
         uint256 _tritium
-    ) internal pure returns (Structs.ERC20s memory) {
+    ) public pure returns (Structs.ERC20s memory) {
         Structs.ERC20s memory cost;
         cost.steel = _steel * quantity;
         cost.quartz = _quartz * quantity;
@@ -25,33 +25,33 @@ contract Defences {
         return (cost);
     }
 
-    function _blasterRequirements(uint256 dockyardLevel) internal pure {
+    function blasterRequirements(uint256 dockyardLevel) public pure {
         require(dockyardLevel >= 1, "Level 1 Dockyard is required");
     }
 
-    function _beamRequirements(
+    function beamRequirements(
         uint256 dockyardLevel,
         Structs.Techs memory techs
-    ) internal pure {
+    ) public pure {
         require(dockyardLevel >= 2, "Level 2 Dockyard is required");
         require(techs.energyInnovation >= 2, "Level 2 Energy tech required");
         require(techs.beamTechnology >= 3, "Level 3 Beam Tech required");
     }
 
-    function _astralLauncherRequirements(
+    function astralLauncherRequirements(
         uint256 dockyardLevel,
         Structs.Techs memory techs
-    ) internal pure {
+    ) public pure {
         require(dockyardLevel >= 6, "Level 6 Dockyard is required");
         require(techs.energyInnovation >= 6, "Level 6 Energy tech required");
         require(techs.armourInnovation >= 3, "Level 3 Armour tech required");
         require(techs.shieldTech >= 1, "Level 1 Shield Tech required");
     }
 
-    function _plasmaBeamRequirements(
+    function plasmaProjectorRequirements(
         uint256 dockyardLevel,
         Structs.Techs memory techs
-    ) internal pure {
+    ) public pure {
         require(dockyardLevel >= 8, "Level 8 Dockyard is required");
         require(techs.plasmaEngineering >= 7, "Level 7 Plasma tech required");
     }
