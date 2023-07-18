f4061280cbccbbcbfd4ea04d90ec8e245ecedfd4
ametel01 <alex-metelli@gmx.com>
2023-07-18 14:06:33 +0800

    added techs Ids

---
+++ src/libraries/ID.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19 .0;
+
+library ID {
+    uint256 public constant ENERGY_INNOVATION = 1;
+    uint256 public constant DIGITAL_SYSTEMS = 2;
+    uint256 public constant BEAM_TECHNOLOGY = 3;
+    uint256 public constant ION_SYSTEMS = 4;
+    uint256 public constant PLASMA_ENGINEERING = 5;
+    uint256 public constant SPACETIME_WARP = 6;
+    uint256 public constant COMBUSTIVE_DRIVE = 7;
+    uint256 public constant THRUST_PROPULSION = 8;
+    uint256 public constant WARP_DRIVE = 9;
+    uint256 public constant ARMOUR_INNOVATION = 10;
+    uint256 public constant ARMS_DEVELOPMENT = 11;
+    uint256 public constant SHIELD_TECH = 12;
+}
